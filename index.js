ea602a83deb518a438dfceb9cd0662dd36d81a2c
Eli Cusic <tntitans31@gmail.com>
2020-11-26 09:47:32 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objArray){
+  let result = objArray.find(obj => obj.result === 'W')
+  if (objArray.every(obj => obj.result !== 'W')){
+    return undefined
+  } else {
+    return result.year
+  }
+
+}
